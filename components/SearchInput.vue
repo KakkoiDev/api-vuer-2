d02e18459e4ef70301b771d5cb172c757675e168
KakkoiDev <contact@kakkoi.dev>
2024-06-27 02:24:25 +0200

    index: add search input

---
+++ components/SearchInput.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// const props = defineProps<{
+//   placeholder: string;
+//   value: string;
+//   reference: string;
+//   inputRefs: Record<string, HTMLInputElement>;
+//   class: string;
+//   search: string;
+// }>();
+
+// defineEmits<{
+//   clearSearch: [reference: string];
+//   getResults: [value: string];
+//   update: [search: string];
+// }>();
+
+const props = defineProps(["class"]);
+
+const model = defineModel();
+</script>
+
+<template>
+  <div class="relative" :class="class">
+    <input
+      type="text"
+      class="rounded-md border-gray-900 px-5 py-3 shadow-sm w-full focus:outline-none placeholder-gray-500"
+      v-model="model"
+    />
+    <Icon
+      name="mdi:close-thick"
+      class="absolute top-3 right-4 cursor-pointer w-6 h-6"
+      title="Clear Search"
+      @click="model = ''"
+    />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const search = ref("init");
+</script>
+
+<template>
+  <section>
+    <AppTitle />
+    <div class="text-lg text-center mt-6">Do you have an API about:</div>
+    <SearchInput class="mt-3" v-model="search" />
+    {{ search }}
+  </section>
+  <section>test</section>
+</template>
